c016325272781f709ecd497562cf5e68278a5989
pedsm <peumendonca@gmail.com>
2017-10-21 17:10:39 +0100

    Fixed menu bar and packagelock

--- src/app.js
+++ src/app.js
@@ -7,31 +7,32 @@ let win
 process.env.GOOGLE_API_KEY = 'AIzaSyC8AEkXwGA3XqX44eSaQlZTtaCsPkSEmVA'
 
 function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600 })
+    win = new BrowserWindow({ width: 800, height: 600 })
+    win.setMenu(null)
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true,
-  }))
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true,
+    }))
 
-  // win.webContents.openDevTools()
+    // win.webContents.openDevTools()
 
-  win.on('closed', () => {
-    win = null
-  })
+    win.on('closed', () => {
+        win = null
+    })
 }
 
 app.on('ready', createWindow)
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
+    if (win === null) {
+        createWindow()
+    }
 })
 
