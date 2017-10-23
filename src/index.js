72d02fe297dd5e7673f98e9e292248cd541e8679
pedsm <peumendonca@gmail.com>
2017-10-23 03:06:07 +0100

    Added base layout on react and version bump

--- src/index.js
+++ src/index.js
@@ -1,19 +1,85 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
-import { Button } from 'antd'
+import styles from './styles'
+import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
+import { Menu, Button, Icon, Modal } from 'antd'
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props)
+        console.log(styles)
+        const low = navigator.geolocation.getCurrentPosition((position) => {
+            this.setState({
+                coords: {
+                    lat: position.coords.latitude,
+                    lon: position.coords.longitude
+                }
+            })
+        })
+        this.state = {
+            coords: {
+                lat: 0,
+                lon: 0
+            },
+            map: null
+        }
+    }
+    
+    start = (map) => {
+        this.setState({ map })
+        console.log(map)
+    }
+
+    handleClick = (e) => {
+
+    }
+
     render() {
-        return(
-            <div>
-                <div id="sidebar"></div>
-                    <Button>Hello</Button>
-                <div id="map">
+        const { root, sidebar, map } = styles;
+        const params = {
+            v: '3.exp',
+            key: 'AIzaSyAvI1of8u5aDRBmUSDAsLPy6BfUtAx5Lws'
+        } 
+
+        return (
+            <div style={root}>
+                <Modal visible={true}>
+                    Get request
+                </Modal>
+                <div id="sidebar" style={sidebar}>
+                    <img src="img/logo.png" alt="" style={{width: sidebar.width, padding: 20}} />
+                    <Menu>
+                        <Menu.Item onclick={this.handleClick}>
+                            <Icon type="download" />
+                            Get request
+                        </Menu.Item>
+                        <Menu.Item onclick={this.handleClick}>
+                            <Icon type="download" />
+                            Post request
+                        </Menu.Item>
+                        <Menu.Divider />
+                        <Menu.Item onclick={this.handleClick}>
+                            <Icon type="hdd" />
+                            File
+                        </Menu.Item>
+                    </Menu>
+                </div>
+                <div id="map" style={map}>
+                    <Gmaps 
+                        params={params}
+                        width={'100%'}
+                        height={'100%'}
+                        zoom={12}
+                        lat={this.state.coords.lat}
+                        lng={this.state.coords.lon}
+                        onMapCreated={this.start}
+                    >
+                    </Gmaps>
                 </div>
             </div>
         )
     }
 }
 
+// eslint-ignore-next-line
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/styles.js
@@ -0,0 +1,15 @@
+const styles = {
+    root: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    sidebar: {
+        width: 150
+    },
+    map: {
+        width: `calc(100vw - 150px)`,
+        height: `100vh`
+    },
+}
+
+export default styles
