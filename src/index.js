9e3aa730e83f76b128aef1c1a3fb8e80ec6a9a0a
pedsm <peumendonca@gmail.com>
2017-10-23 03:11:25 +0100

    Added title

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ export default class App extends React.Component {
 
         return (
             <div style={root}>
-                <Modal visible={true}>
+                <Modal visible={true} title={'Add Data'}>
                     Get request
                 </Modal>
                 <div id="sidebar" style={sidebar}>
